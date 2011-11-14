c8a80752fdd5a0a15a60e2bde9d41aae4d5a2794
fumikito <guy@hametuha.com>
2011-11-14 08:44:06 +0000

    オプション設定ページの表示順を変更 refs #954 @0.1h
    git-svn-id: http://svn.hametuha.org/public/literally-wordpress/trunk@103 87317609-05d5-4675-b73b-d7da3dd9357e

--- admin/setting.php
+++ admin/setting.php
@@ -107,46 +107,6 @@
 			</tr>
 		</tbody>
 	</table>
-	<h3><?php $this->e('Transfer Setting'); ?></h3>
-	<table class="form-table">
-		<tbody>
-			<tr>
-				<th valign="top">
-					<label><?php $this->e('Accept Transfer'); ?></label>
-				</th>
-				<td>
-					<label><input type="radio" name="transfer" value="0" <?php if(!$this->option['transfer']) echo 'checked="checked"'; ?> /><?php $this->e('Disallow'); ?></label><br />
-					<label><input type="radio" name="transfer" value="1" <?php if($this->option['transfer']) echo 'checked="checked"'; ?> /><?php $this->e('Allow'); ?></label>
-					<p class="description">
-						<?php $this->e('If you accept transfer, users can pay with bank account or something that is not digital transaction.'); ?>
-						<?php $this->e('This helps users, but transactional process has a little bit more complex, because you have to check actual bank account to know whether bank deposit transfer has been made.'); ?>	
-						<small>（<?php echo $this->help("transfer", $this->_("More &gt;"))?>）</small>
-					</p>
-				</td>
-			</tr>
-			<tr>
-				<th><label><?php $this->e('Notification Frequency'); ?></label></th>
-				<td>
-					<label>
-						<?php printf(
-								$this->_('Send reminder on every %s days'),
-								'<input class="short" type="text" name="notification_frequency" id="notification_frequency" value="'.intval($this->option['notification_frequency']).'" />'
-						);?>
-					</label><br />
-					<label>
-						<?php printf(
-								$this->_('Transaction expires by %s days'),
-								'<input class="short" type="text" name="notification_limit" id="notification_limit" value="'.intval($this->option['notification_limit']).'" />'
-						);?>
-					</label>
-					<p class="description">
-						<?php $this->e('If you don\'t want to send reminder, set notification frequency to 0. Transfer transaction will be expired after notification limit days will have been past.'); ?><br />
-					</p>
-				</td>
-			</tr>
-		</tbody>
-	</table>
-	
 	<h3><?php $this->e('WordPress Setting'); ?></h3>
 	<table class="form-table">
 		<tbody>
@@ -276,6 +236,46 @@
 			</tr>
 		</tbody>
 	</table>
+	<h3><?php $this->e('Transfer Setting'); ?></h3>
+	<table class="form-table">
+		<tbody>
+			<tr>
+				<th valign="top">
+					<label><?php $this->e('Accept Transfer'); ?></label>
+				</th>
+				<td>
+					<label><input type="radio" name="transfer" value="0" <?php if(!$this->option['transfer']) echo 'checked="checked"'; ?> /><?php $this->e('Disallow'); ?></label><br />
+					<label><input type="radio" name="transfer" value="1" <?php if($this->option['transfer']) echo 'checked="checked"'; ?> /><?php $this->e('Allow'); ?></label>
+					<p class="description">
+						<?php $this->e('If you accept transfer, users can pay with bank account or something that is not digital transaction.'); ?>
+						<?php $this->e('This helps users, but transactional process has a little bit more complex, because you have to check actual bank account to know whether bank deposit transfer has been made.'); ?>	
+						<small>（<?php echo $this->help("transfer", $this->_("More &gt;"))?>）</small>
+					</p>
+				</td>
+			</tr>
+			<tr>
+				<th><label><?php $this->e('Notification Frequency'); ?></label></th>
+				<td>
+					<label>
+						<?php printf(
+								$this->_('Send reminder on every %s days'),
+								'<input class="short" type="text" name="notification_frequency" id="notification_frequency" value="'.intval($this->option['notification_frequency']).'" />'
+						);?>
+					</label><br />
+					<label>
+						<?php printf(
+								$this->_('Transaction expires by %s days'),
+								'<input class="short" type="text" name="notification_limit" id="notification_limit" value="'.intval($this->option['notification_limit']).'" />'
+						);?>
+					</label>
+					<p class="description">
+						<?php $this->e('If you don\'t want to send reminder, set notification frequency to 0. Transfer transaction will be expired after notification limit days will have been past.'); ?><br />
+					</p>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	
 	<p class="submit">
 		<input type="submit" class="button-primary" value="<?php $this->e("Update"); ?>" />
 	</p>
