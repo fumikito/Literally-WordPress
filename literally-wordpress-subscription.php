a1a7ef238ba3d407123c71752f4f9bcc2e35fc5e
fumikito <guy@hametuha.com>
2011-12-12 06:11:22 +0000

    定期購読者以外のコンテンツを入れ替える closes #1014 @2h
    git-svn-id: http://svn.hametuha.org/public/literally-wordpress/trunk@117 87317609-05d5-4675-b73b-d7da3dd9357e

--- literally-wordpress-subscription.php
+++ literally-wordpress-subscription.php
@@ -39,15 +39,15 @@ class LWP_Subscription {
 	public function __construct($option) {
 		$option = shortcode_atts(array(
 			'subscription' => false,
-			'subscription_post_type' => array(),
+			'subscription_post_types' => array(),
 			'subscription_format' => 'all'
 		), $option);
 		$this->enabled = (boolean)$option['subscription'];
-		$this->post_types = (array)$option['subscription_post_type'];
+		$this->post_types = (array)$option['subscription_post_types'];
 		switch($option['subscription_format']){
 			case 'more':
 			case 'nextpage':
-				$this->format = $option['subscription_fomrat'];
+				$this->format = $option['subscription_format'];
 				break;
 			default:
 				$this->format = 'all';
@@ -57,6 +57,8 @@ class LWP_Subscription {
 			add_action('init', array($this, 'register_post_type'));
 			add_action('admin_init', array($this, 'admin_init'));
 			add_action('edit_post', array($this, 'edit_post'));
+			add_filter('the_content', array($this, 'the_content'));
+			add_shortcode('lwp_subscribe', array($this, 'shortcode'));
 		}
 	}
 	
@@ -173,6 +175,91 @@ class LWP_Subscription {
 			update_post_meta($post_id, '_lwp_subscription_price', (int)$_REQUEST['subscription_price']);
 			update_post_meta($post_id, '_lwp_subscription_expires', (int)$_REQUEST['subscription_expires']);
 		}
+	}
+	
+	
+	/**
+	 * Filter for the_content
+	 * @param string $content
+	 * @return string
+	 */
+	public function the_content($content){
+		if(!is_admin() && $this->enabled && false !== array_search(get_post_type(), $this->post_types) && !$this->is_subscriber()){
+			//Get invitation message
+			$message = get_page_by_path($this->invitation_slug, 'OBJECT', $this->post_type);
+			remove_filter('the_content', array($this, 'the_content'));
+			$append = '<div class="lwp-invitation">'.apply_filters('the_content', $message->post_content).'</div>';
+			//Get current page infomation
+			global $page, $pages;
+			if($page > count($pages)){
+				$page = count($pages);
+			}
+			switch($this->format){
+				case 'more':
+					$more_page = 0;
+					foreach($pages as $p){
+						$more_page++;
+						if(preg_match('/<!--more(.*?)?-->/', $p)){
+							break;
+						}
+					}
+					if($page == $more_page){
+						$page_content = preg_split("/<!--more(.*?)?-->/", $pages[$page - 1]);
+						$content = apply_filters('the_content', $page_content[0]).$append;
+					}elseif($page > $more_page){
+						$content = $append;
+					}
+					break;
+				case 'nextpage':
+					//if is paged and page is > 1, check if current user is subscriber.
+					if($page > 1){
+						$content = '';
+					}
+					$content .= $append;
+					break;
+				default:
+					$content = $append;
+					break;
+			}
+			add_filter('the_content', array($this, 'the_content'));
+		}
+		return $content;
+	}
+	
+	/**
+	 * Returns if specified user is subascriber
+	 * @global Literally_WordPress $lwp
+	 * @global wpdb $wpdb
+	 * @global int $user_ID
+	 * @param int $user_ID
+	 * @return boolean 
+	 */
+	public function is_subscriber($user_ID = null){
+		global $lwp, $wpdb;
+		if(is_null($user_ID)){
+			global $user_ID;
+		}
+		$sql = <<<EOS
+			SELECT * FROM {$lwp->transaction} AS t
+			INNER JOIN {$wpdb->posts} AS p
+			ON t.book_id = p.ID
+			WHERE t.status = %s AND t.user_id = %d AND p.post_type = %s
+			  AND ((t.expires = '0000-00-00 00:00:00') OR (t.expires > %s))
+EOS;
+		return (boolean)$wpdb->get_row($wpdb->prepare($sql, LWP_Payment_Status::SUCCESS, $user_ID, $this->post_type, gmdate('Y-m-d H:i:s')));
+	}
+	
+	/**
+	 * Returns shortcode
+	 * @param array $atts
+	 * @param string $content
+	 * @return string
+	 */
+	public function shortcode($atts, $content = ''){
+		$atts = shortcode_atts(array(
+			
+		), $atts);
+		return admin_url();
 	}
 	
 	/**
