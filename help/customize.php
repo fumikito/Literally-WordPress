9698b459843ee04d24d005e2d805ffaefe27c62a
Takahashi Fumiki <guy@hametuha.com>
2012-04-23 18:23:39 +0900

    ダウンロードリストのサンプルコード追加 close #3

--- help/customize.php
+++ help/customize.php
@@ -42,9 +42,93 @@
 	<li><?php printf($lwp->_('Customize theme. The template tag lwp_buy_now() will display button. See detail at <a href="%s" target="_blank">function reference</a>.'), $lwp->url.'docs/'); ?></li>
 </ol>
 <h2><?php $lwp->e('Customize Device list and Download list'); ?></h2>
+<h3><?php $lwp->e('Device list'); ?></h3>
+<p><?php $lwp->e('Device list is a simple list of registered devices. It helps informing your user if your products are available or not. Especially, digital contents such as ebook, audio or video needs appropriate devices to play, so you should display the device list. If you want display device list manually, code is below:'); ?></p>
+<pre class="brush: php; html-script: true">
+&lt;?php
+	//<?php $lwp->e('Get device'); ?>
+
+	$devices = lwp_get_devices();
+	foreach($devices as $d):
+		//<?php $lwp->e('Process each device. <code>Try var_dump($d)</code>.'); ?>
+
+?&gt;
+	&lt;div class="device&lt;?php echo ($d["valid"]) ? ' valid' : ' invalid';?&gt;"&gt;
+		&lt;small class="center sans"&gt;&lt;?php echo $d["valid"] ? "<?php $lwp->e('OK');  ?>" : "<?php $lwp->e('NG'); ?>"; ?&gt;&lt;/small&gt;
+		&lt;img src="&lt;?php bloginfo('template_directory'); ?&gt;/img/ebook-devices/&lt;?php echo $d["slug"]; ?&gt;.png" alt="&lt;?php echo $d["name"]; ?&gt;" width="48" height="48"/&gt;
+		&lt;span class="sans center"&gt;&lt;?php echo $d["name"]; ?&gt;&lt;/span&gt;
+	&lt;/div&gt;
+&lt;?php endforeach; ?&gt;
+</pre>
+<h3><?php $lwp->e('Download list'); ?></h3>
 <p>
-	<?php $lwp->e('Coming soon...'); ?>
+	<?php $lwp->e('Download list is a simple list of registered files. You may provide multiple files as one product for various reason. Saving bandwidth, Providing trial file, and so on. Download list helps your customer to test what they can archive. To display download list manually, see the code blow:'); ?>
 </p>
+<pre class="brush: php; html-script: true">
+&lt;?php
+	//<?php $lwp->e('You get file list and loop it.'); ?>
+
+	$files = lwp_get_files();
+	foreach($files as $f):
+		$ext = lwp_get_ext($f); //<?php $lwp->e('You can get file extention.'); ?>
+
+		$devices = lwp_get_file_devices($f); //<?php $lwp->e('You can get device list in array with which the file is available.'); ?>
+			
+		$accessibility = lwp_get_accessibility($f); //<?php $lwp->e('This tells you who can access this file.'); ?>
+			
+?&gt;
+	&lt;div class="device-container"&gt;
+		&lt;div class="file-meta"&gt;
+			&lt;strong class="file-title "&gt;&lt;?php echo $f-&gt;name; ?&gt;&lt;/strong&gt;
+			&lt;small&gt;（&lt;?php lwp_get_date($f); ?&gt;）&lt;/small&gt;
+		&lt;/div&gt;
+		&lt;div class="desc"&gt;
+			&lt;?php echo wpautop($f-&gt;desc); ?&gt;
+			&lt;small&gt;Available with：&lt;?php echo implode(', ', $devices); ?&gt;&lt;/small&gt;&lt;br /&gt;
+			&lt;small&gt;File size: &lt;?php echo lwp_get_size($f); ?&gt;&lt;/small&gt;
+		&lt;/div&gt;
+		&lt;div class="download"&gt;
+			&lt;?php
+				//<?php $lwp->e('If downloadable, display link.'); ?>
+
+				if(
+					($accessibility == "owner" && lwp_is_owner() ) //<?php $lwp->e('File is only for owner and current user is owner.'); ?>
+						
+					||
+					($accessibility == "member" && is_user_logged_in()) //<?php $lwp->e('File is downloadable for every registered user.'); ?>
+						
+					||
+					($accessibility == "any") //<?php $lwp->e('Any wan can download.'); ?>
+						
+				):
+			?&gt;
+				&lt;a href="&lt;?php echo lwp_file_link($f-&gt;ID); ?&gt;"&gt;
+					<?php $lwp->e('Download'); ?>
+
+				&lt;/a&gt;
+			&lt;?php else: /* <?php $lwp->e('If not, show how to download.'); ?> */?&gt;
+				&lt;span class="not-downloadable"&gt;
+					<?php $lwp->e('Cannot download.'); ?>
+
+					&lt;?php
+						switch($accessibility){
+							case "owner":
+								echo "<?php $lwp->e('Downloadable after purchase'); ?>";
+								break;
+							case "member":
+								echo "<?php $lwp->e('Only member can download'); ?>";
+								break;
+							default:
+								echo "<?php $lwp->e('Unavailable'); ?>";
+								break;
+						}
+					?&gt;
+				&lt;/span&gt;
+			&lt;?php endif; ?&gt;
+		&lt;/div&gt;
+&lt;?php endforeach; ?&gt;
+</pre>
+
 <h2><?php $lwp->e('Customize Form'); ?></h2>
 <p>
 	<?php $lwp->e('Coming soon...'); ?>
--- help/index.php
+++ help/index.php
@@ -1,14 +1,27 @@
 <?php
-
+/* @var $lwp Literally_WordPress */
 require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))).DIRECTORY_SEPARATOR."wp-load.php";
 
 if(isset($_GET["name"])){
 	$path = $lwp->dir.DIRECTORY_SEPARATOR."help".DIRECTORY_SEPARATOR.str_replace(".", "", str_replace("/", "", $_GET['name'])).".php";
-	if(file_exists($path)):?><html>
+	$dir = plugin_dir_url(__FILE__);
+	if(file_exists($path)):?><!DOCTYPE html>
+<html>
 <head>
+	<meta charset="utf-8">
 	<title><?php $lwp->e("Literally WordPress Help"); ?></title>
-	
-	<link rel="stylesheet" href="../assets/style.css">
+	<link rel="stylesheet" href="<?php echo $dir; ?>../assets/style.css" type="text/css">
+	<link rel="stylesheet" href="<?php echo $dir; ?>syntax/shCore.css" type="text/css">
+	<link rel="stylesheet" href="<?php echo $dir; ?>syntax/shThemeDefault.css" type="text/css">
+	<script type="text/javascript" src="<?php echo $dir; ?>syntax/shCore.js"></script>
+	<script type="text/javascript" src="<?php echo $dir; ?>syntax/shBrushXml.js"></script>
+	<script type="text/javascript" src="<?php echo $dir; ?>syntax/shBrushPhp.js"></script>
+	<script type="text/javascript">
+		//<![CDATA[
+			alert('hoge');
+			SyntaxHighlighter.all();
+		//]]>
+	</script>
 </head>
 <body>
 	<div class="help-wrap">
