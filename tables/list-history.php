88543f953e01a2e5644aee219d7021e6d76f9c16
Takahashi Fumiki <guy@hametuha.com>
2012-07-04 13:49:51 +0900

    購入履歴のページネーションが公開側でも動くようにする close #69

--- tables/list-history.php
+++ tables/list-history.php
@@ -279,4 +279,83 @@ EOS;
 	function get_table_classes() {
 		return array_merge(parent::get_table_classes(), array('lwp-table'));
 	}
+	
+	function pagination( $which ) {
+		if(is_admin()){
+			parent::pagination($which);
+		}else{
+			if ( empty( $this->_pagination_args ) )
+			return;
+
+			extract( $this->_pagination_args );
+
+			$output = '<span class="displaying-num">' . sprintf( _n( '1 item', '%s items', $total_items ), number_format_i18n( $total_items ) ) . '</span>';
+
+			$current = $this->get_pagenum();
+
+			$current_url = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+			$current_url = preg_replace("/page\/[0-9].*\/?$/", "", remove_query_arg( array( 'hotkeys_highlight_last', 'hotkeys_highlight_first' ), $current_url ));
+
+			$page_links = array();
+
+			$disable_first = $disable_last = '';
+			if ( $current == 1 )
+				$disable_first = ' disabled';
+			if ( $current == $total_pages )
+				$disable_last = ' disabled';
+
+			$page_links[] = sprintf( "<a class='%s' title='%s' href='%s'>%s</a>",
+				'first-page' . $disable_first,
+				esc_attr__( 'Go to the first page' ),
+				esc_url( remove_query_arg( 'paged', $current_url ) ),
+				'&laquo;'
+			);
+
+			$page_links[] = sprintf( "<a class='%s' title='%s' href='%s'>%s</a>",
+				'prev-page' . $disable_first,
+				esc_attr__( 'Go to the previous page' ),
+				esc_url( add_query_arg( 'paged', max( 1, $current-1 ), $current_url ) ),
+				'&lsaquo;'
+			);
+
+			if ( 'bottom' == $which )
+				$html_current_page = $current;
+			else
+				$html_current_page = sprintf( "<input class='current-page' title='%s' type='text' name='%s' value='%s' size='%d' />",
+					esc_attr__( 'Current page' ),
+					esc_attr( 'paged' ),
+					$current,
+					strlen( $total_pages )
+				);
+
+			$html_total_pages = sprintf( "<span class='total-pages'>%s</span>", number_format_i18n( $total_pages ) );
+			$page_links[] = '<span class="paging-input">' . sprintf( _x( '%1$s of %2$s', 'paging' ), $html_current_page, $html_total_pages ) . '</span>';
+
+			$page_links[] = sprintf( "<a class='%s' title='%s' href='%s'>%s</a>",
+				'next-page' . $disable_last,
+				esc_attr__( 'Go to the next page' ),
+				esc_url( add_query_arg( 'paged', min( $total_pages, $current+1 ), $current_url ) ),
+				'&rsaquo;'
+			);
+
+			$page_links[] = sprintf( "<a class='%s' title='%s' href='%s'>%s</a>",
+				'last-page' . $disable_last,
+				esc_attr__( 'Go to the last page' ),
+				esc_url( add_query_arg( 'paged', $total_pages, $current_url ) ),
+				'&raquo;'
+			);
+
+			$output .= "\n<span class='pagination-links'>" . join( "\n", $page_links ) . '</span>';
+
+			if ( $total_pages )
+				$page_class = $total_pages < 2 ? ' one-page' : '';
+			else
+				$page_class = ' no-pages';
+
+			$this->_pagination = "<div class='tablenav-pages{$page_class}'>$output</div>";
+
+			echo $this->_pagination;
+		}
+	}
 }
